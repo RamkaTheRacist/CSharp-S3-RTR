ac6cd3de1d11aa7f0e3742b702fca32d81e7ff77
Ramka <whereisourgod@gmail.com>
2022-05-16 11:12:39 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+int tmp = 1;
+int res = 1;
+while(tmp <= number)
+{
+    res = res * tmp; tmp++;
+}
+Console.WriteLine($"Result is {res}");
