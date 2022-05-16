a6b73cb4b2dccdf3127f6ec2d5d1b5246cf5fa29
Ramka <whereisourgod@gmail.com>
2022-05-16 10:16:40 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+Console.Clear();
+int tmp = 1;
+while(tmp <= number)
+{
+    Console.WriteLine(tmp * tmp); tmp++;
+}
