6c783c4f7617ef23ead6ef3990a9767b4b9b47b7
Ramka <whereisourgod@gmail.com>
2022-05-16 11:07:16 +0300

    f_task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,24 +1,19 @@
 ﻿//Определить количество цифр в числе
-//Console.WriteLine("Insert ur number");
-//int number = int.Parse(Console.ReadLine());
-//int count = 0;
-//int tmpnumber = number;
-//while(tmpnumber > 0)
-//{
-    //tmpnumber = tmpnumber / 10;
-    //count++;
-//}
-//Console.WriteLine($"Digits in {number} is {count}");
-
-
 Console.WriteLine("Insert ur number");
 int number = int.Parse(Console.ReadLine());
-string c;
-c = number.ToString();
-int c1 = c.Length;
-Console.WriteLine($"Digits in {number} is {c1}");
+int count = 0;
+int tmpnumber = number;
+while(tmpnumber > 0)
+{
+    tmpnumber = tmpnumber / 10;
+    count++;
+}
+Console.WriteLine($"Digits in {number} is {count}");
 
 
-//int a = int.Parse(Console.ReadLine());
-//string b = a.ToString();
-//Console.WriteLine($"Третья цефра числа = {b[2]}")
+//Console.WriteLine("Insert ur number");
+//int number = int.Parse(Console.ReadLine());
+//string c;
+//c = number.ToString();
+//int c1 = c.Length;
+//Console.WriteLine($"Digits in {number} is {c1}");
