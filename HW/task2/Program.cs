c2f898f5b63d3938386fa639cbd29eea90f1157b
Ramka <whereisourgod@gmail.com>
2022-05-19 12:45:53 +0300

    task2

---
+++ HW/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Найти кубы чисел от 1 до N
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+for(int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i}^3 = {i*i*i}");
+}
