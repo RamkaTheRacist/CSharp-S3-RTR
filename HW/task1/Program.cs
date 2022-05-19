09c9c2d6511c2b99d104c48ada874c15f056f6e5
Ramka <whereisourgod@gmail.com>
2022-05-19 12:31:26 +0300

    task1

--- HW/task1/Program.cs
+++ HW/task1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+for(int i = 1; i <= number; i++)
+{
+    for(int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
