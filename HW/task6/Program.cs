c463fb639a4cc02af7ac404030a0b52a26341178
Ramka <whereisourgod@gmail.com>
2022-05-19 13:29:17 +0300

    task6

---
+++ HW/task6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+void check(int number)
+{
+    int tmp = number * number * number;
+    int digit = tmp % 10;
+    if(digit % 2 == 0)
+    {
+    Console.WriteLine($"{tmp}");
+    return;
+    }
+    else
+    {
+    Console.WriteLine("Nope");    
+    return;
+    }
+}
+check(number);
