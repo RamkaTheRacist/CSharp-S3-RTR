93dcac2b187abfc9b13196f8d5b46511841c667f
Ramka <whereisourgod@gmail.com>
2022-05-19 12:59:43 +0300

    task4

---
+++ HW/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Exponentiation?");
+int exp = int.Parse(Console.ReadLine());
+int res = number;
+if(exp > 0)
+{
+for(int i = 2; i <= exp; i++)
+{
+    res = res * number;
+}
+Console.WriteLine($"{number}^{exp} = {res}");
+}
+else if(exp == 0)
+{
+    Console.WriteLine($"{number}^{exp} = 0");
+}
+else
+{
+    Console.WriteLine("Exponentiation shouldnt be less than 0");
+}
