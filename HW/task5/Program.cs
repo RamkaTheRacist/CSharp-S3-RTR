1e1896d00450e0ddfd387dd69accc70d437feb86
Ramka <whereisourgod@gmail.com>
2022-05-19 13:13:25 +0300

    task5

---
+++ HW/task5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Подсчитать сумму цифр в числе
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+int digit = 0;
+if(number > 0)
+{
+    while(number > 0)
+    {
+    digit= number % 10;
+    sum = sum + digit;
+    number = number / 10;
+    Console.WriteLine($"Result is {sum}");
+    }
+}
+else if(number == 0)
+{
+Console.WriteLine($"Result is {number}");
+}
+else
+{
+    while(number < 0)
+    {
+    digit= number % 10;
+    sum = sum + digit;
+    number = number / 10;
+    Console.WriteLine($"Result is {sum}");
+    }  
+}
