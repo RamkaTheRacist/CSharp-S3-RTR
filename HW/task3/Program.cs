51b29b84930e5f9a98ecc34974bc0fae5cc470e3
Ramka <whereisourgod@gmail.com>
2022-05-19 12:48:24 +0300

    task3

---
+++ HW/task3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Найти сумму чисел от 1 до А
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+int res = 0;
+for(int i = 1; i <= number; i++)
+{
+    res = res + i;
+}
+Console.WriteLine($"Result is {res}");
